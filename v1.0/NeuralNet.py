2408ed273dbfc5b6b1519ea2f730d0dd5deb64c9
Jayesh Anil <90074472+jay-esh@users.noreply.github.com>
2023-02-23 18:35:17 -0500

    Update NeuralNet.py

--- v1.0/NeuralNet.py
+++ v1.0/NeuralNet.py
@@ -89,47 +89,6 @@ pickle_out.close()
 # pred = NN.predict(x_test)
 # print(pred)
 
-
-# while True:
-#     try:
-
-#     except EOFError:
-#         break
-
-# Step 2
-# # Loading the dataset
-# dataset = load_digits()
-
-# # Step 3
-# # Splitting the data into tst and train
-# # 80 - 20 Split
-# x_train, x_test, y_train, y_test = train_test_split(
-#     dataset.data, dataset.target, test_size=0.20, random_state=4)
-
-# # Step 4
-# # Making the Neural Network Classifier
-# NN = MLPClassifier()
-
-# # Step 5
-# # Training the model on the training data and labels
-# NN.fit(x_train, y_train)
-
-# # Step 6
-# # Testing the model i.e. predicting the labels of the test data.
-# y_pred = NN.predict(x_test)
-
-# # Step 7
-# # Evaluating the results of the model
-# accuracy = accuracy_score(y_test, y_pred)*100
-# confusion_mat = confusion_matrix(y_test, y_pred)
-
-# # Step 8
-# # Printing the Results
-# print("Accuracy for Neural Network is:", accuracy)
-# print("Confusion Matrix")
-# print(confusion_mat)
-
-
 # class NeuralNet:
 #     def __init__(self) -> None:
 #         pass
